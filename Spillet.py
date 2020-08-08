9cf3c99be02c16c35f2db76006fddc8177e97315
Benedicte April Jansen <dicteaj@Benedictes-MacBook-Air.local>
2020-08-08 14:43:45 +0200

    spillet

---
+++ Spillet.py
@@ -0,0 +1,23 @@
+
+import pygame
+pygame.init()
+
+
+bredde = 500
+høyde = 500
+screen = pygame.display.set_mode([bredde, høyde])
+
+running = True
+while running:
+ # fiks de andre quit greiene også
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((255, 255, 255)) #hvit bakgrunn
+
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)  #bare en sikrel
+
+    pygame.display.flip()
+
+pygame.quit()
